6b95d43d6796cc9bbe2966eeccabc0cad92598ed
Sourcery AI <>
2022-04-01 11:54:11 +0000

    'Refactored by Sourcery'

--- mangle2mask.py
+++ mangle2mask.py
@@ -26,7 +26,7 @@ import pymangle
 from astropy.io import fits
 from astropy import wcs
     
-def mask_creator (ra_ctr, dec_ctr, fov_x, fov_y, pixSize, mangle_fn):  
+def mask_creator(ra_ctr, dec_ctr, fov_x, fov_y, pixSize, mangle_fn):  
 
     # Define the number of pixels of the output mask (AMICO style)
     nx = int(math.floor(fov_x/pixSize + 1))
@@ -38,14 +38,14 @@ def mask_creator (ra_ctr, dec_ctr, fov_x, fov_y, pixSize, mangle_fn):
     w.wcs.crval = [ra_ctr,dec_ctr]
     w.wcs.cdelt = np.array([pixSize, pixSize])
     w.wcs.ctype = ["RA---TAN", "DEC--TAN"]
-    
+
     # Read the mangle polygons describing regions to be masked using pymangle
     mask = pymangle.Mangle(mangle_fn)
 
     # Loop over the mask image
     m = np.arange(nx*ny).reshape(ny,nx)
-    for i in range(0, nx):
-        for j in range(0, ny):
+    for i in range(nx):
+        for j in range(ny):
             # Transform to celestial coordinates
             this_ra, this_dec = w.wcs_pix2world(i, j, 0)
             m[j,i] = int(1 - mask.contains(this_ra,this_dec) + 0.5)
@@ -63,15 +63,15 @@ if total != 7+1:
     print ("Error: wrong number of arguments")
     print ('  usage: mangle2mask <ra> <dec> <fov_x> <fov_y> <pixSize> <mangle_fn> <output_fn>')
     sys.exit(1)
-    
+
 ra = float(sys.argv[1])
-dec = float(sys.argv[2]) 
+dec = float(sys.argv[2])
 fov_x = float(sys.argv[3])
 fov_y = float(sys.argv[4])
 pixSize = float(sys.argv[5])
 mangle_fn = str(sys.argv[6])
 output_fn = str(sys.argv[7])
-print ("  executing: %s " % cmdargs)
+print(f"  executing: {cmdargs} ")
 
 # Execute
 m, h = mask_creator (ra, dec, fov_x, fov_y, pixSize, mangle_fn)
@@ -81,6 +81,6 @@ hdu = fits.PrimaryHDU (m, header=h)
 hdu.writeto (output_fn)
 
 
-print ('Done!')        
+print ('Done!')
 sys.exit(0)
 
